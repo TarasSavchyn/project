7d5bfb9e732355de710df2fed9c27de10a033772
Taras Savchyn <savik1992@gmail.com>
2024-02-28 01:45:11 +0200

    implememt JWT Token

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', "localhost", "127.0.0.1",]
 
 
 # Application definition
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "app.apps.AppConfig",
     "user",
 ]
