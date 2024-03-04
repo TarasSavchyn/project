a4dcf057e70130602c64eb646521161a57b7b23d
Taras Savchyn <savik1992@gmail.com>
2024-03-05 01:27:26 +0200

    set SIMPLE_JWT settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 import dotenv
@@ -133,4 +134,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination."
                                 "PageNumberPagination",
     "PAGE_SIZE": 25,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=31),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=31),
 }
