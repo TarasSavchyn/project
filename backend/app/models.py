ed476fe16e553ea2088c105a38b71a2359eef695
Taras Savchyn <savik1992@gmail.com>
2024-03-04 16:31:26 +0200

    add models Post, Comment, SubComment

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    comments = models.ForeignKey(
+        "Comment",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="comment_post",
+    )
+
+    def __str__(self):
+        return f"Post by {self.user.username} at {self.created_at}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_comments"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="post_comments"
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.email} on Post {self.post.id}"
+
+class SubComment(models.Model):
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+        related_name="sub_comment"
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_sub_comments"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="post_sub_comments"
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SubComment {self.user.name} on post {self.post.content[:25]}"
+
+
+
