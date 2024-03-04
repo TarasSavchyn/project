37730b18ce4d88e7e2e8c267c8488cd1af4f3a2f
Taras Savchyn <savik1992@gmail.com>
2024-03-04 16:45:27 +0200

    add Post, Comment, SubComment to admin.py

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import (
+    Post,
+    Comment,
+    SubComment,
+)
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(SubComment)
